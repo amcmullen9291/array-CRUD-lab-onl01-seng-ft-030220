a8641f2eb460eca9cd5653856130fef1c50eb94f
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-04 09:13:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   houston=["wow", "spring", "summer", "arrays!"]
-  houston.shift(0)
+  houston.shift(wow)
 end
 
 def retrieve_element_from_index(array, index_number)
