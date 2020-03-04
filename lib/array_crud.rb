e1add6b24807fc462f80644f32776158d9481e7b
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-04 09:15:03 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  houston=[]
 end
 
 def create_an_array
-  
+  houston=["winter", "spring", "summer", "fall"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    houston=["winter", "spring", "summer", "fall"]
+    houston.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  houston=["winter", "spring", "summer", "fall"]
+  houston.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  houston=["winter", "spring", "summer", "arrays!"]
+  houston.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  houston=["wow", "spring", "summer", "arrays!"]
+  houston.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    houston=["winter", "spring", "am", "fall"]
+    return houston[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    houston=["wow", "spring", "summer", "fall"]
+    return houston[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    houston=["winter", "spring", "summer", "arrays!"]
+    return houston[-1]
 end
