fcc215831f7d6df1e52db5a185ec66bea52584c9
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-04 09:10:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-    houston=["winter", "spring", "summer", "fall"]
+    houston=["winter", "spring", "summer", "arrays!"]
     return houston[-1]
 end
